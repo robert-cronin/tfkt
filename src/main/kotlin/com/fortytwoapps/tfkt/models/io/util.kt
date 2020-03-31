74202c3f1a53ab18db4e13c1bb85a13291b83752
Robbie Cronin <robert.cronin@uqconnect.edu.au>
2020-03-31 20:30:37 +0800

    intermediary updates

--- src/main/kotlin/com/fortytwoapps/tfkt/models/io/util.kt
+++ src/main/kotlin/com/fortytwoapps/tfkt/models/io/util.kt
@@ -13,11 +13,11 @@ private fun Json.toMap(): Map<String, Any?> {
 
 // Management
 suspend fun copyModel(sourceUrl: String, destURL: String): Map<String, Any?> {
-    return com.fortytwoapps.tfkt.data.copyModelJS(sourceUrl, destURL).await().toMap()
+    return copyModelJS(sourceUrl, destURL).await().toMap()
 }
 suspend fun listModels(): Map<String, Any?> {
-    return com.fortytwoapps.tfkt.data.listModelsJS().await().toMap()
+    return listModelsJS().await().toMap()
 }
 suspend fun removeModel(url: String): Map<String, Any?> {
-    return com.fortytwoapps.tfkt.data.removeModelJS(url).await().toMap()
+    return removeModelJS(url).await().toMap()
 }
